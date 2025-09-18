8cf894051b10e99c418249ec0a3b35b43fb9362e
Lucasmenezes08 <lms4@cesar.school>
2025-09-18 19:58:52 -0300

    :lipstick: style: Estilizar componente removeBtn.

--- UI_gerenciador_extensoes/src/components/App.tsx
+++ UI_gerenciador_extensoes/src/components/App.tsx
@@ -4,7 +4,7 @@ import Header from "./header/header"
 function App() {
 
   return (
-    <section className="px-25 pt-10 w-full h-screen flex flex-col bg-linear-to-b from-black/95 to-indigo-950 font-sans overflow-auto gap-10">
+    <section className="px-25 pt-10 pb-10 w-full h-screen flex flex-col bg-linear-to-b from-black/95 to-indigo-950 font-sans overflow-auto gap-10">
       <Header/>
       <section className="">
           <ExtensionList/>
--- UI_gerenciador_extensoes/src/components/extensions/extensionList/commonExtensionUI/removeBtn.tsx
+++ UI_gerenciador_extensoes/src/components/extensions/extensionList/commonExtensionUI/removeBtn.tsx
@@ -1,6 +1,6 @@
 export function RemoveBtn (){
     return (
-        <button>
+        <button className="flex items-center justify-center bg-gray-900/70 px-5 rounded-2xl text-white/80 text-sm border-1 border-solig border-gray-500 cursor-pointer">
             Remove
         </button>
     )
